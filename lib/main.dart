6c4b17df804907722c20cef384f9d2b88e186431
inamullah <inamullah.oraxtech@gmail.com>
2021-05-28 15:53:04 +0500

    onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeal',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amber,
-            canvasColor: Color.fromRGBO(255, 254, 229, 1),
-            fontFamily: 'Raleway',
-            textTheme: ThemeData.light().textTheme.copyWith(
-                body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                title: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.bold))),
-        // home: CategoriesScreen(),
-        initialRoute: '/categories-screen',
-        routes: {
-          '/': (ctx) => CategoriesScreen(),
-          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        });
+      title: 'DeliMeal',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold))),
+      // home: CategoriesScreen(),
+      initialRoute: '/categories-screen',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        // MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
     //{
     //   '/categories-screen': (_) => CategoriesScreen(),
     //   '/categories-meals': (ctx) => CategoryMealsScreen(),
