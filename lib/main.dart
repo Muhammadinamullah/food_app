e3614a1e0d7c5ba35b00462e492585471098aa1f
inamullah <inamullah.oraxtech@gmail.com>
2021-05-28 16:08:43 +0500

    onUnknownRoute to handle the unknown routes

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeal',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              title: TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'RobotoCondensed',
-                  fontWeight: FontWeight.bold))),
-      // home: CategoriesScreen(),
-      initialRoute: '/categories-screen',
-      routes: {
-        '/': (ctx) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-        // MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-      },
-      onGenerateRoute: (settings) {
-        print(settings.arguments);
-        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
-      },
-    );
+        title: 'DeliMeal',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.amber,
+            canvasColor: Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                title: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'RobotoCondensed',
+                    fontWeight: FontWeight.bold))),
+        // home: CategoriesScreen(),
+        initialRoute: '/categories-screen',
+        routes: {
+          '/': (ctx) => CategoriesScreen(),
+          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+          // MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        },
+        // onGenerateRoute: (settings) {
+        //   print(settings.arguments);
+        //   if (settings.name == '/meal-detail') {
+        //     return ... ;
+        //   }else if(settings.name == '/something-else'){
+        //     return ...;
+        //   }
+        //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+        // },
+        onUnknownRoute: (settings) {
+          return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+        });
     //{
     //   '/categories-screen': (_) => CategoriesScreen(),
     //   '/categories-meals': (ctx) => CategoryMealsScreen(),
